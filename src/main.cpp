a5f8b752345e827f86a6e2ea5cd06ea145ca8768
Glitchpix <davidrannaleet@gmail.com>
2024-11-05 13:55:19 +0100

    +Initial ppm image "writing" working

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 int main() {
-  std::cout << "Hello world!" << std::endl;
-  return 0;
+  constexpr int imageWidth = 256;
+  constexpr int imageHeight = 256;
+
+  constexpr double maxColorValue = 255.0;
+
+  std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+  for (int yIndex = 0; yIndex < imageHeight; ++yIndex) {
+    for (int xIndex = 0; xIndex < imageWidth; ++xIndex) {
+      auto r = double(xIndex) / (imageWidth - 1);
+      auto g = double(yIndex) / (imageHeight - 1);
+      auto b = 0.0;
+
+      int ir = static_cast<int>(round(maxColorValue * r));
+      int ig = static_cast<int>(round(maxColorValue * g));
+      int ib = static_cast<int>(round(maxColorValue * b));
+
+      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+    }
+  }
 }
