c948b9cf2c5ec045da07eef3faae7698dd119878
Glitchpix <davidrannaleet@gmail.com>
2024-11-07 14:18:34 +0100

    +Add reflectance with Schlick’s approximation

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,8 @@ int main() {
 
   auto materialGround = std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
   auto materialCenter = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
-  auto materialLeft = std::make_shared<Dielectric>(1.0 / 1.333);
+  auto materialLeft = std::make_shared<Dielectric>(1.51);
+  auto materialBubble = std::make_shared<Dielectric>(1.00 / 1.51);
   auto materialRight = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 1.0);
 
   world.add(
@@ -18,6 +19,8 @@ int main() {
   world.add(
       std::make_shared<Sphere>(Vec3(0.0, 0.0, -1.2), 0.5, materialCenter));
   world.add(std::make_shared<Sphere>(Vec3(-1.0, 0.0, -1.0), 0.5, materialLeft));
+  world.add(
+      std::make_shared<Sphere>(Vec3(-1.0, 0.0, -1.0), 0.4, materialBubble));
   world.add(std::make_shared<Sphere>(Vec3(1.0, 0.0, -1.0), 0.5, materialRight));
 
   constexpr double aspectRatio = 16.0 / 9.0;
--- src/material.hpp
+++ src/material.hpp
@@ -77,7 +77,8 @@ public:
 
     bool totalInternalReflection = refractIndex * sinTheta > 1.0;
     Vec3 newDirection{};
-    if (totalInternalReflection) {
+    if (totalInternalReflection ||
+        reflectance(cosTheta, refractIndex) > utils::randomDouble()) {
       newDirection = reflect(incomingUnitDirection, hitInfo.normal);
     } else {
       newDirection =
@@ -90,4 +91,10 @@ public:
 
 private:
   double mRefractionIndex{};
+  static double reflectance(double cosine, double refraction_index) {
+    // Use Schlick's approximation for reflectance.
+    auto r0 = (1 - refraction_index) / (1 + refraction_index);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+  }
 };
