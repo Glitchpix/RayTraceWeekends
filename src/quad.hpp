992b39bb7e9ce7e5bf97b63f674e41c2cc6b3a72
Glitchpix <davidrannaleet@gmail.com>
2024-11-15 16:23:08 +0100

    +Some initial hit implementation, not complete yet

--- src/quad.hpp
+++ src/quad.hpp
@@ -2,6 +2,7 @@
 
 #include "hittable.hpp"
 #include "material.hpp"
+#include "vec3.hpp"
 #include <memory>
 
 class Quad : public Hittable {
@@ -9,17 +10,35 @@ public:
   Quad(const Vec3& position, const Vec3& widthVector, const Vec3& heightVector,
        std::shared_ptr<IMaterial> material)
       : mPosition{position}, mWidthVector{widthVector},
-        mHeightVector{heightVector}, mMaterial{material} {
+        mHeightVector{heightVector},
+        mNormal{unitVector(cross(mWidthVector, mHeightVector))},
+        mDistanceFromOrigin{dot(mPosition, mNormal)}, mMaterial{material} {
     setBoundingBox();
   }
 
   bool hit(const Ray& ray, Interval rayRange,
            HitRecord& hitInfo) const override {
-    (void)ray;
-    (void)rayRange;
-    (void)hitInfo;
-    // TODO
-    return false;
+    const auto rayPlaneAngle = dot(ray.direction(), mNormal);
+
+    if (std::fabs(rayPlaneAngle) < kEpsilon) {
+      return false;
+    }
+
+    const auto t =
+        (mDistanceFromOrigin - dot(mNormal, ray.origin())) / rayPlaneAngle;
+
+    if (!rayRange.contains(t)) {
+      return false;
+    }
+
+    const auto intersection = ray.at(t);
+
+    hitInfo.t = t;
+    hitInfo.position = intersection;
+    hitInfo.material = mMaterial;
+    hitInfo.setFaceNormal(ray, mNormal);
+
+    return true;
   }
 
   [[nodiscard]] AABB boundingBox() const override { return mBoundingBox; }
@@ -28,8 +47,12 @@ private:
   Vec3 mPosition;
   Vec3 mWidthVector;
   Vec3 mHeightVector;
+  Vec3 mNormal;
+  double mDistanceFromOrigin{0};
   std::shared_ptr<IMaterial> mMaterial;
   AABB mBoundingBox;
+
+  static constexpr double kEpsilon{1e-8};
 
   void setBoundingBox() {
     const auto firstDiagonal =
